a56331e1b4d27d2aa9179a0ea1da14e74170d422
Romina D-R <rdr2711@yahoo.com>
2018-06-22 21:04:41 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -14,9 +14,17 @@ function linkSelector() {
 }
 
 function imageSelector() {
+<<<<<<< HEAD
   return $('img[alt="cat sleeping"]')
 }
 
 function checkboxInputSelector() {
  return $(" :checkbox") 
+=======
+  return $(":cat sleeping")
+}
+
+function checkboxInputSelector() {
+ return $("type = checkbox") 
+>>>>>>> bdf9f81374471a30c3ebc3570f88f29ead9e2542
 }
